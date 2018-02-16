8fd5531d70a208a9c7bde3536125e6eb3b9f614c
Lachlan Douglas <lachlan@engines.org>
2018-02-16 11:31:18 +1100

    new images

--- start.rb
+++ start.rb
@@ -28,17 +28,17 @@ end
 
 def my_work_pages
   [
-    [ "TechSydney - Proposal", "Jordan Lane Creative", 5 ],
-    [ "CARAS - Brochure & Stationery", "CARAS", 6 ],
-    [ "Hark Attack - Brochure & Collateral", "Hark Attack", 4 ],
-    [ "LAIDRE - Brochure", "LAIDRE", 4 ],
-    [ "Hark Attack - CC’s Advertisements", "Hark Attack", 3 ],
-    [ "AMZ Engineering - Brochure", "AMZ Engineering", 4 ],
-    [ "Techfront - Brochure", "Jordan Lane Creative", 4 ],
-    [ "Identikit - Book Covers", "Identikit", 3 ],
-    [ "AMIC - Conference Collateral", "AMIC", 5 ],
-    [ "AMPC - Annual Report", "AMPC", 6 ],
-    [ "Corporate ID", "Various", 6 ],
-    [ "Photography", "Personal", 17 ],
+    [ "TechSydney - Proposal", "Jordan Lane Creative", "Graphic Design", 5 ],
+    [ "CARAS - Brochure & Stationery", "CARAS", "Graphic Design", 4 ],
+    [ "Hark Attack - Brochure & Collateral", "Hark Attack", "Graphic Design", 4 ],
+    [ "LAIDRE - Brochure", "LAIDRE", " Graphic Design & Photography", 4 ],
+    [ "Hark Attack - CC’s Advertisements", "Hark Attack", " Graphic Design & Photo Retouching", 3 ],
+    [ "AMZ Engineering - Brochure", "AMZ Engineering", "Graphic Design & Photography", 4 ],
+    [ "Techfront - Brochure", "Jordan Lane Creative", "Graphic Design", 4 ],
+    [ "Identikit - Book Covers", "Identikit", "Photography", 3 ],
+    [ "AMIC - Conference Collateral", "AMIC", "Graphic Design", 5 ],
+    [ "AMPC - Annual Report", "AMPC", "Graphic Design", 6 ],
+    [ "Corporate ID", "Various", "Graphic Design", 6 ],
+    [ "Photography", "Personal", "Photography", 16 ],
   ]
 end
