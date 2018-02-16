0bc601c66b33d946591d8b2f74e49ebe3d2dddd4
Lachlan Douglas <lachlan@engines.org>
2018-02-16 13:37:28 +1100

    tidy contact me

--- public/assets/application.js
+++ public/assets/application.js
@@ -1,12 +1,6 @@
 $( document ).ready(
 
-  var bMobile =   // will be true if running on a mobile device
-    navigator.userAgent.indexOf( "Mobile" ) !== -1 ||
-    navigator.userAgent.indexOf( "iPhone" ) !== -1 ||
-    navigator.userAgent.indexOf( "Android" ) !== -1 ||
-    navigator.userAgent.indexOf( "Windows Phone" ) !== -1 ;
-
-  if (bMobile) {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $(".mobile_friendly").show();
     $(".mobile_unfriendly").hide();
   } else {
